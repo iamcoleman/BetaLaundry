997cd9426781ce4738f33617d74860fe020986f1
Coleman Lyski <coleman.lyski@gmail.com>
2019-01-17 15:27:51 -0500

    Firebase and Vue.js

---
+++ beta-laundry/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>Coleman Waz Here</h1>
+  <p>Data 1: {{ data1 }}</p>
+  <p>Data 2: {{ data2 }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data1: "Hairy Balls",
+      data2: "Stinky Gooch"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
