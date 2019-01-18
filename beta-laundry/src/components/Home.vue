10318fa09edf178c81153810e9c1a84fd837f67a
Coleman Lyski <coleman.lyski@gmail.com>
2019-01-18 15:26:08 -0500

    Got the database access working

--- beta-laundry/src/components/Home.vue
+++ beta-laundry/src/components/Home.vue
@@ -11,7 +11,16 @@
 
         <v-divider></v-divider>
 
-        <v-card-text></v-card-text>
+        <v-card-text>
+          <v-layout row wrap>
+            <v-flex xs6 pa-2>
+              <p>Status: {{ leftWasher.open }}</p>
+            </v-flex>
+            <v-flex xs6 pa-2>
+              <p>Estimated Time Remaining</p>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
       </v-card>
     </v-flex>
 
@@ -24,7 +33,16 @@
 
         <v-divider></v-divider>
 
-        <v-card-text></v-card-text>
+        <v-card-text>
+          <v-layout row wrap>
+            <v-flex xs6 pa-2>
+              <p>Status: {{ rightWasher.open }}</p>
+            </v-flex>
+            <v-flex xs6 pa-2>
+              <p>Estimated Time Remaining</p>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
       </v-card>
     </v-flex>
 
@@ -37,7 +55,16 @@
 
         <v-divider></v-divider>
 
-        <v-card-text></v-card-text>
+        <v-card-text>
+          <v-layout row wrap>
+            <v-flex xs6 pa-2>
+              <p>Status: {{ leftDryer.open }}</p>
+            </v-flex>
+            <v-flex xs6 pa-2>
+              <p>Estimated Time Remaining</p>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
       </v-card>
     </v-flex>
 
@@ -51,8 +78,14 @@
         <v-divider></v-divider>
 
         <v-card-text>
-          <p>Status: {{ rightDryer.open }}</p>
-          <p>gotDatabase: {{ gotDatabase }}</p>
+          <v-layout row wrap>
+            <v-flex xs6 pa-2>
+              <p>Status: {{ rightDryer.open }}</p>
+            </v-flex>
+            <v-flex xs6 pa-2>
+              <p>Estimated Time Remaining</p>
+            </v-flex>
+          </v-layout>
         </v-card-text>
       </v-card>
     </v-flex>
@@ -87,17 +120,51 @@ export default {
   },
 
   created() {
-    this.gotDatabase = getFullDatabase();
+    this.databaseConnection();
+  },
+
+  methods: {
+    databaseConnection() {
+      var vueGuy = this;
+      var refLeftWasher = firebase.database().ref("/leftWasher");
+      var refRightWasher = firebase.database().ref("/rightWasher");
+      var refLeftDryer = firebase.database().ref("/leftDryer");
+      var refRightDryer = firebase.database().ref("/rightDryer");
+
+      refLeftWasher.on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var childData = childSnapshot.val();
+          // console.log(childData);
+          vueGuy.leftWasher.open = childData;
+        });
+      });
+
+      refRightWasher.on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var childData = childSnapshot.val();
+          // console.log(childData);
+          vueGuy.rightWasher.open = childData;
+        });
+      });
+
+      refLeftDryer.on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var childData = childSnapshot.val();
+          // console.log(childData);
+          vueGuy.leftDryer.open = childData;
+        });
+      });
+
+      refRightDryer.on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var childData = childSnapshot.val();
+          // console.log(childData);
+          vueGuy.rightDryer.open = childData;
+        });
+      });
+    }
   }
 };
-
-function getFullDatabase() {
-  return firebase.database().ref('/rightWasher').once('value').then(function(snapshot) {
-    var db = snapshot.val() || 'Anonymous';
-    console.log(db);
-    return db;
-  });
-}
 </script>
 
 <style scoped>
