61575e8cfcffcacd176294901382242b1d000f9f
Coleman <coleman.lyski@gmail.com>
2019-10-27 18:33:30 -0400

    add all files from old computer

--- beta-laundry-groupme/src/bot.ts
+++ beta-laundry-groupme/src/bot.ts
@@ -1,12 +1,49 @@
+import axios from 'axios';
+
 export class Bot {
-  static async respond(req: any, res: any) {
+  static async respond(req: any, res: any, databaseRef: any) {
     const request = JSON.parse(req.chunks[0]);
 
-    // console logs
-    console.log(request);
-    console.log(res);
+    if (request.text.toLowerCase().includes('/laundry')) {
+      console.log('Text contains /laundry');
+
+      // get the values from the database
+      databaseRef.once('value')
+          .then(snapshot => {
+            return snapshot.toJSON();
+          })
+          .then(data => {
+            this.sendGroupMeMessage(data);
+          })
+          .catch(e => {
+            console.log(`Firebase Error: ${ e }`);
+          });
+    } else {
+      console.log('Text does not contain /laundry ');
+    }
 
     res.writeHead(200);
     res.end();
+  }
+
+  static async sendGroupMeMessage(data: any) {
+    const responseText = `Left Washer: ${data.leftWasher.open ? 'Open' : 'Taken'}
+Right Washer: ${data.rightWasher.open ? 'Open' : 'Taken'}
+Left Dryer: ${data.leftDryer.open ? 'Open' : 'Taken'}
+Right Dryer: ${data.rightDryer.open ? 'Open' : 'Taken'}`;
+
+    const messageRequest = {
+      bot_id: process.env.BOT_ID,
+      text: responseText
+    };
+
+    try {
+      await axios.post('https://api.groupme.com/v3/bots/post', messageRequest);
+      console.log(messageRequest);
+      return true;
+    } catch (e) {
+      console.log(`Axios Error: ${ e }`);
+      return false;
+    }
   }
 }
--- beta-laundry-groupme/src/index.ts
+++ beta-laundry-groupme/src/index.ts
@@ -1,9 +1,32 @@
 require('dotenv').config();
 import express from 'express';
+import * as firebase from "firebase/app";
+import "firebase/database";
 import { Bot } from './bot';
 
-const port = Number(process.env.PORT || 5000);
 
+/*
+ * Setup Firebase
+ */
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "beta-laundry.firebaseapp.com",
+  databaseURL: "https://beta-laundry.firebaseio.com",
+  projectId: "beta-laundry",
+  storageBucket: "beta-laundry.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const databaseRef = firebase.database().ref();
+
+
+/*
+ * Setup express
+ */
+const port = Number(process.env.PORT || 5000);
 const app = express();
 
 // Get the request data from the GroupMe callback
@@ -24,7 +47,7 @@ app.get( "/", (req, res) => {
 });
 app.post('/', (req, res) => {
   // Send the GroupMe callback data to the Bot respond function
-  Bot.respond(req, res)
+  Bot.respond(req, res, databaseRef)
 });
 
 // start the Express server
